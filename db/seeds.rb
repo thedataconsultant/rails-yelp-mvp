c245c12e1e32505b2270a008c2974a5b4ae4a192
Nate Gunawan <nategunawan@gmail.com>
2020-11-24 17:40:08 +0800

    next

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+moana = Restaurant.new(name: "Moana", category: "japanese", address: "618 Bali")
+two_moods = Restaurant.new(name: "Two Moods", category: "italian", address: "420 Bali")
+milk_and_madu = Restaurant.new(name: "Milk and Madu", category: "belgian", address: "999 Bali")
+la_brisa = Restaurant.new(name: "La Brisa", category: "french", address: "1 Bali")
+hamatora = Restaurant.new(name: "Hamatora", category: "chinese", address: "43 Bali")
+
+moana.save
+two_moods.save
+milk_and_madu.save
+la_brisa.save
+hamatora.save
